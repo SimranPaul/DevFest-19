bbd96a5bd803414a43527f87af79d8b939d94d67
Simran Paul <simranpaul33@gmail.com>
2019-09-29 11:00:57 +0530

    Tab View added

--- lib/main.dart
+++ lib/main.dart
@@ -29,30 +29,55 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {},
-          ),
-          actions: <Widget>[
-           Notif(),
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {},
-            )
-          ],
-          title: Text("Bookings"),
-          backgroundColor: Color.fromRGBO(27, 27, 27, 100),
-        ),
-        backgroundColor: Color.fromRGBO(12, 12, 12, 100),
-        body: ListView(
-          children: <Widget>[
-           
-            Placard("Hawaii   to   Sevilla", "29th Sept 2019", "Emirates Airways"),
-            Placard("Manipal  to   Goa", "20th Sept 2019", "Thivim Rails"),
-            Placard("Sevilla  to   Morroco", "30th Sept 2019", "Emirates Airways")
-          ],
-        ));
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+            appBar: AppBar(
+              leading: IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {},
+              ),
+              bottom: TabBar(
+                tabs: <Widget>[
+                  Tab(
+                    child:
+                        Text("TRIPS", style: TextStyle(color: Colors.blueGrey)),
+                  ),
+                  Tab(
+                    child: Text("HOTELS",
+                        style: TextStyle(color: Colors.blueGrey)),
+                  ),
+                ],
+              ),
+              actions: <Widget>[
+                Notif(),
+                IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {},
+                )
+              ],
+              title: Text("Bookings"),
+              backgroundColor: Color.fromRGBO(27, 27, 27, 100),
+            ),
+            backgroundColor: Color.fromRGBO(12, 12, 12, 100),
+            body: TabBarView(children: <Widget>[
+              ListView(
+                children: <Widget>[
+                  Placard("Hawaii   to   Sevilla", "29th Sept 2019",
+                      "Emirates Airways"),
+                  Placard(
+                      "Manipal  to   Goa", "20th Sept 2019", "Thivim Rails"),
+                  Placard("Sevilla  to   Morroco", "30th Sept 2019",
+                      "Emirates Airways")
+                ],
+              ),
+              Container(
+                child: Text(
+                  "No Hotels Available!",
+                  style: TextStyle(color: Colors.blueGrey),
+                ),
+                alignment: Alignment.center,
+              )
+            ])));
   }
 }
